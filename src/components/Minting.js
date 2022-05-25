24c6c1ee64b40210edbc0891c72071b950744400
WahyuS002 <syahputrawahyu61@gmail.com>
2022-05-25 21:33:53 +0700

    :package: install all web3 package

--- src/components/Minting.js
+++ src/components/Minting.js
@@ -20,22 +20,22 @@ export default function Minting() {
                             <button>
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
-                                    class="h-6 w-6 text-gray-400 hover:text-primary transition-all duration-300 ease-in-out"
+                                    className="h-6 w-6 text-gray-400 hover:text-primary transition-all duration-300 ease-in-out"
                                     viewBox="0 0 20 20"
                                     fill="currentColor"
                                 >
-                                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 000 2h6a1 1 0 100-2H7z" clip-rule="evenodd" />
+                                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 000 2h6a1 1 0 100-2H7z" clipRule="evenodd" />
                                 </svg>
                             </button>
                             <span className="text-xl">1</span>
                             <button>
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
-                                    class="h-6 w-6 text-gray-400 hover:text-primary transition-all duration-300 ease-in-out"
+                                    className="h-6 w-6 text-gray-400 hover:text-primary transition-all duration-300 ease-in-out"
                                     viewBox="0 0 20 20"
                                     fill="currentColor"
                                 >
-                                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
+                                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clipRule="evenodd" />
                                 </svg>
                             </button>
                         </span>
@@ -56,11 +56,11 @@ export default function Minting() {
                         <span>Contract</span>
                         <div className="flex items-center">
                             <span className="mr-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-blue-300" viewBox="0 0 20 20" fill="currentColor">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-300" viewBox="0 0 20 20" fill="currentColor">
                                     <path
-                                        fill-rule="evenodd"
+                                        fillRule="evenodd"
                                         d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                                        clip-rule="evenodd"
+                                        clipRule="evenodd"
                                     />
                                 </svg>
                             </span>
